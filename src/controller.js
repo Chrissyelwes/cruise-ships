cd81504be42aa9c158c39bdcbee6816f9f6aeebe
Chrissy Elwes <96088605+Chrissyelwes@users.noreply.github.com>
2022-03-24 20:13:43 +0000

    Encapsulated JS files objects

--- src/controller.js
+++ src/controller.js
@@ -1,44 +1,43 @@
 (function exportController() {
-
-function Controller () {
-    this.initialiseSea();
-};
-
-Controller.prototype = {
-initialiseSea() {
-    const backgrounds = [
-        'images/water0.png',
-        'images/water1.png',
-    ];
-    let backgroundIndex = 0;
-    window.setInterval(() => {
-        document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
-        backgroundIndex += 1;
-    }, 1000);
-},
+    function Controller () {
+        this.initialiseSea();
 }
 
 Controller.prototype = {
+    initialiseSea() {
+        const backgrounds = [
+            'images/water0.png',
+            'images/water1.png',
+        ];
+        let backgroundIndex = 0;
+            window.setInterval(() => {
+                document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
+            backgroundIndex += 1;
+        }, 1000);
+    },
+
     renderPorts(ports) {
-    const portsElement = document.querySelecter('#ports');
-    portsElement.style.width = '0px';
-    ports.forEach( (port, index) => {
-        const newPortElement = document.createElement('div');
-        newPortElement.className = 'port';
-        newPortElement.dataset.portName = port.portName;
-        newPortElement.dataset.portIndex = index;
-        portsElement.appendChild(newPortElement);
+        const portsElement = document.querySelector('#ports');
+        portsElement.style.width = '0px';
 
-        const portsElementWidth = parseInt(portsElement.style.width, 10);
-        portsElement.style.width = `${portsElementWidth + 256}px`;
-    })
+        ports.forEach( (port, index) => {
+            const newPortElement = document.createElement('div');
 
+            newPortElement.className = 'port';
+            newPortElement.dataset.portName = port.portName;
+            newPortElement.dataset.portIndex = index;
+
+            portsElement.appendChild(newPortElement);
+
+            const portsElementWidth = parseInt(portsElement.style.width, 10);
+            portsElement.style.width = `${portsElementWidth + 256}px`;
+        });
+}
+}
 
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = Controller;
 } else {
     window.Controller = Controller;
-}
-    }
 }
 }());
